b5d78eece5e1d2422c809955392d2342b6ddbe70
Sean Brown <seanedbrown@gmail.com>
2025-02-13 20:07:18 -0700

    Finished home for now.

--- source/home/home.jsx
+++ source/home/home.jsx
@@ -1,9 +1,38 @@
 import React from "react";
+import './home.css'
 
 export function Home() {
     return (
-        <main>
-            <div>Home displayed here</div>
-        </main>
+        <div className="container-fluid">
+            <div className="demo-box">
+                <nav>
+                    <h3>Saved Cats<span className="margin-left-80"><button className="btn btn-outline-dark">Sign Out</button></span></h3>
+                </nav>
+                    <section>
+                        <p>
+                            <h5>Placeholder for DataBase</h5>
+                            <table className="table">
+                                <thread>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Diagnosis</th>
+                                    </tr>
+                                </thread>
+                                <tbody>
+                                    <tr>
+                                        <td>cat name</td>
+                                        <td>cancer</td>
+                                    </tr>
+                                    <tr>
+                                        <td>cat name</td>
+                                        <td>cancer</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </p>
+                    </section>
+                    <button className="btn btn-outline-dark">add new cat</button>
+                </div>
+            </div>
     );
 }
